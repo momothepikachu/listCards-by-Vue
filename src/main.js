7590380b1a2eadfafd97f52466e38bdea9ea3b63
Monica Rao <monicaxrao@gmail.com>
2019-04-01 15:55:21 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import BaseIcon from '@/components/BaseIcon.vue'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
 
+const requireComponent = require.context('./components', false, /Base[A-Z]\w+\.(vue|js)$/)
+
+//Automatica Global Component Registration
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+  )
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
+//Single Global Component Registration
 Vue.component('BaseIcon', BaseIcon)
 
 Vue.config.productionTip = false
